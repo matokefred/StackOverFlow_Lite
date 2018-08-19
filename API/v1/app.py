a31fbdf02ccb45f84dfbf96489fd5a367b7df874
matokefred <fredrickalpha96@gmail.com>
2018-08-19 07:24:10 +0300

    User is able to delete a question

--- API/v1/app.py
+++ API/v1/app.py
@@ -89,6 +89,14 @@ def add_answer(uri):
     return jsonify({'You answered this question': qn[0]})
 
 
+@app.route('/questions/<int:uri>', methods=['DELETE'])
+def delete_question(uri):
+    qn = [qn for qn in questions if qn['uri'] == uri]
+    if len(qn) == 0:
+        abort(400)
+    questions.remove(qn[0])
+    return jsonify({'success!': 'The question has been deleted successfully'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
