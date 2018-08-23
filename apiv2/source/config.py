8ca37355e1f0441ee18a4e31a48b4cd58119b656
Matoke24 <fredrickmatoke24@gmail.com>
2018-08-23 16:47:24 +0300

    Improvements on the app file

--- apiv2/source/config.py
+++ apiv2/source/config.py
@@ -11,22 +11,28 @@ class Development(object):
     JWT_VERIFY_EXPIRATION = True
     JWT_EXPIRATION_DELTA = timedelta(seconds=1200)
     DB_NAME = 'apidb'
-    DATABASE_HOST = 'localhost'
-    DATABASE_PASSWORD = ''
+    DB_HOST = 'localhost'
+    DB_PASSWORD = ''
     DB_USER = 'postgres'
     TRACK_MODIFICATIONS = True
+    JWT_AUTH_URL_RULE = '/apiv12/auth/login'
+    JWT_AUTH_USERNAME_KEY = 'email'
+    JWT_AUTH_ENDPOINT = 'login'
 
 
 class Testing(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'simplicityisthegame'
     JWT_VERIFY_EXPIRATION = True
     DB_NAME = 'apidb'
-    DATABASE_HOST = 'localhost'
-    DATABASE_PASSWORD = ''
+    DB_HOST = 'localhost'
+    DB_PASSWORD = ''
     DB_USER = 'postgres'
     TRACK_MODIFICATIONS = True
     DEBUG = True
     JWT_EXPIRATION_DELTA = timedelta(seconds=1200)
+    JWT_AUTH_URL_RULE = '/apiv2/auth/login'
+    JWT_AUTH_USERNAME_KEY = 'email'
+    JWT_AUTH_ENDPOINT = 'login'
 
 
 APP_CONFIG = {
