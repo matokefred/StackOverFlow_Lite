f7ae6c853ce25eaa41d0f140993546d783cf12e6
Matoke24 <fredrickmatoke24@gmail.com>
2018-08-23 08:10:18 +0300

    Added variables to the config file

--- apiv2/source/config.py
+++ apiv2/source/config.py
@@ -1,22 +1,35 @@
 '''Import os so that the secret protection key can be gotten from the environment'''
 import os
+from datetime import timedelta
 
 
 class Development(object):
     DEBUG = True
     TESTING = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY') or 'simplicityisthegame'
     DATABASE_URI = os.getenv('DATABASE_URL')
+    JWT_VERIFY_EXPIRATION = True
+    JWT_EXPIRATION_DELTA = timedelta(seconds=1200)
+    DATABASE_NAME = 'apidb'
+    DATABASE_HOST = 'localhost'
+    DATABASE_PASSWORD = ''
+    DATABASE_USER = 'postgres'
+    TRACK_MODIFICATIONS = True
 
 
-class Production(object):
-    DEBUG = False
-    TESTING = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-    DATABASE_URI = os.getenv('DATABASE_URL')
+class Testing(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'simplicityisthegame'
+    JWT_VERIFY_EXPIRATION = True
+    DATABASE_NAME = 'apidb'
+    DATABASE_HOST = 'localhost'
+    DATABASE_PASSWORD = ''
+    DATABASE_USER = 'postgres'
+    TRACK_MODIFICATIONS = True
+    DEBUG = True
+    JWT_EXPIRATION_DELTA = timedelta(seconds=1200)
 
 
 APP_CONFIG = {
     'development': Development,
-    'production': Production
+    'testing': Testing
 }
