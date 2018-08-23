cb934a87678c3699c5b65dcbc378825b6b184c9b
Matoke24 <fredrickmatoke24@gmail.com>
2018-08-23 11:33:44 +0300

    Update the run file

--- apiv2/run.py
+++ apiv2/run.py
@@ -1,8 +1,6 @@
-import os
-
 from source.app import APP
 
-if __name__ == '__main__':  # If the file and main names are the same
-    '''Call the function and pass our environment name'''
-    define_env = os.getenv('FLASK_ENV')
+
+if __name__ == '__main__':
+    APP.run(debug=True)
 
