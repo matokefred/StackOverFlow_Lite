c1a193d24ea99b05f0807935018b9801714382a2
Matoke24 <fredrickmatoke24@gmail.com>
2018-08-23 14:49:14 +0300

    User is able to add a question

--- apiv2/source/end/endpoints.py
+++ apiv2/source/end/endpoints.py
@@ -61,7 +61,8 @@ def get_one_question(question_id):
         }]
         cursor.close()
         return jsonify(output)
-    return abort(404)
+    else:
+        return abort(404)
 
 
 @PRINTS.route('/apiv2/questions', methods=['POST'])
@@ -74,7 +75,8 @@ def post_question():
         CONNECT.commit()
         cursor.close()
         return jsonify({"Success!": "Your question has been recorded"})
-    abort(400)
+    else:
+        return abort(400)
 
 
 @PRINTS.route('/apiv2/questions/<int:question_id>', methods=['POST'])
@@ -90,7 +92,29 @@ def answer_question(question_id):
             cursor.execute(sqlcode, (int(current_identity), request.json['answer'], question_id))
             CONNECT.commit()
             return jsonify({"Congratulations": "Answer received"})
-        return abort(400)
-    return abort(404)
+        else:
+            return abort(400)
+    else:
+        return abort(404)
 
 
+@PRINTS.route('/apiv1/questions/<int: question_id>', methods=['DELETE'])
+@jwt_required
+def delete_question(question_id):
+    cursor = CONNECT.cursor()
+    sqlcode = 'SELECT * FROM questions WHERE questionId=%s;'
+    cursor.execute(sqlcode, ([question_id]))
+    qn = cursor.fetchall()
+    if qn:
+        if int(current_identity)is qn[0][1]:
+            sqlcode = 'DELETE FROM answers WHERE questionId=%s;'
+            cursor.execute(sqlcode, ([question_id]))
+            sqlcode = 'DELETE FROM questions WHERE questionId=%s;'
+            cursor.execute(sqlcode, ([question_id]))
+            cursor.close()
+            CONNECT.commit()
+            return jsonify({"200": "The question has been deleted!"})
+        else:
+            return jsonify({"403":"That action is not allowed"})
+    else:
+        return abort(404)
